dbea7648d10919184ab1c9e379b9f1bcf3b4f4b2
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-05-14 00:24:19 -0300

    feat: predictions

--- predictions.py
+++ predictions.py
@@ -19,7 +19,10 @@ class PredictXg(d6tflow.tasks.TaskCSVPandas):
         shots = self.input()[0].load()
         model = self.input()[1].load()
 
-        X = shots[model.feature_cols]
+        feature_cols = ['angle', 'distance', 'distance2', 'angle2', 'dist_angle']
+        X = shots[feature_cols]
+        cat_vars = ['prev_action', 'bodypart_name', 'type_name']
+        X = pd.concat([X, pd.get_dummies(shots[cat_vars])], axis=1)
 
         shots['xg'] = model.predict_proba(X)[:, 1]
 
